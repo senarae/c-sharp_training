d6e1414858810118ff2cfaad28c795405fc87c13
senarae <senarae@gmail.com>
2019-07-17 16:51:03 +0000

    Change the hello text

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -6,7 +6,7 @@ namespace MyFirstProject
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!"); 
+            Console.WriteLine("Hello, Sena World!"); 
         }
     }
 }
